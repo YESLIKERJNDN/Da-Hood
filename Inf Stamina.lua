682170e2f0d5d2ed2ed8dc72db364417e3099bd2
Zack <78792046+TinyBuild321@users.noreply.github.com>
2022-07-08 06:52:11 +0800

    Add files via upload

--- Inf Stamina.lua
+++ Inf Stamina.lua
@@ -9,8 +9,6 @@ function InfStamina(Character)
             LP.Character.BodyEffects.Defense.Value = 9e9
         end)
     end)
-    wait(1)
-    game.Players.LocalPlayer.PlayerGui.MainScreenGui.Bar.Energy.bar.Visible = false
 end
 InfStamina(game.Players.LocalPlayer.Character)
 game.Players.LocalPlayer.CharacterAdded:Connect(InfStamina)
