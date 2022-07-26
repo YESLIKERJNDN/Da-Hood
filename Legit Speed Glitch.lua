2768ae14bef63089ebda258641af0b24a27bf02e
Zack <78792046+TinyBuild321@users.noreply.github.com>
2022-07-26 16:51:17 +0800

    Add files via upload

--- Legit Speed Glitch.lua
+++ Legit Speed Glitch.lua
@@ -1,52 +1,114 @@
 if game.PlaceId == 2788229376 then
     setfpscap(360)
-    Key = Enum.KeyCode.Z
-    game:GetService("UserInputService").InputBegan:Connect(function(input,chatting)
-        if input.UserInputType == Enum.UserInputType.Keyboard then
-            if chatting then IsChatting = true else IsChatting = false end
-            if input.KeyCode == Key then
-                if IsChatting ~= false then return end
-                if not toggle then
-                    toggle = true
-                    Instance.new("Animation", game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations")).Name = 'Greet'
-                    game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations"):findFirstChild("Greet").AnimationId = 'rbxassetid://3189777795'
-                    local Greet = game.ReplicatedStorage.ClientAnimations.Greet
-                    local Humanoid = game.Players.LocalPlayer.Character.Humanoid
-                    local Animation = Humanoid:LoadAnimation(Greet)
-                    Animation:Play()
-                    wait(1.6)
-                    for i, v in next, game:GetService("Players").LocalPlayer.Backpack:GetChildren() do
-                        if v:IsA("Tool") and v.Name ~= "Combat" or v.Name ~= "[Boombox]" then
-                            game:GetService("Players").LocalPlayer.Character:findFirstChildOfClass'Humanoid':EquipTool(v)
+
+    local Notification = true
+    local AutoMacro = true
+    local Macro = true
+    local AutoMacroKey = Enum.KeyCode.Z
+    local MacroKey = Enum.KeyCode.X
+
+    if AutoMacro == true then
+        game:GetService("UserInputService").InputBegan:Connect(function(input,chatting)
+            if input.UserInputType == Enum.UserInputType.Keyboard then
+                if chatting then IsChatting = true else IsChatting = false end
+                if input.KeyCode == AutoMacroKey then
+                    if IsChatting ~= false then return end
+                    if not AutoMacroToggle then
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "AutoMacro";
+                                Text = "AutoMacro: On";
+                                Duration = 3;
+                            })
                         end
-                    end
-                    wait()
-                    game:GetService("VirtualInputManager"):SendKeyEvent(true, "S", false, game)
-                    spawn(function()
-                        while toggle == true do wait(0)
-                            game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
-                            game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
-                            game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
-                            game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
-                            game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
-                            game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                        AutoMacroToggle = true
+                        Instance.new("Animation", game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations")).Name = 'Greet'
+                        game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations"):findFirstChild("Greet").AnimationId = 'rbxassetid://3189777795'
+                        local Greet = game.ReplicatedStorage.ClientAnimations.Greet
+                        local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+                        local Animation = Humanoid:LoadAnimation(Greet)
+                        Animation:Play()
+                        wait(1.6)
+                        for i, v in next, game:GetService("Players").LocalPlayer.Backpack:GetChildren() do
+                            if v:IsA("Tool") and v.Name ~= "Combat" or v.Name ~= "[Boombox]" then
+                                game:GetService("Players").LocalPlayer.Character:findFirstChildOfClass'Humanoid':EquipTool(v)
+                            end
                         end
-                    end)
-                    Animation:Stop()
-                    Macro =
-                        game:service'Players'.LocalPlayer.Character:findFirstChildOfClass'Humanoid'.Running:Connect(function()
-                            Animation:Stop()
+                        wait()
+                        game:GetService("VirtualInputManager"):SendKeyEvent(true, "S", false, game)
+                        spawn(function()
+                            while AutoMacroToggle == true do wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                            end
                         end)
-                else
-                    toggle = false
-                    local Greet = game.ReplicatedStorage.ClientAnimations.Greet
-                    local Humanoid = game.Players.LocalPlayer.Character.Humanoid
-                    local Animation = Humanoid:LoadAnimation(Greet)
-                    Animation:Stop()
-                    game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations"):findFirstChild("Greet"):Remove()
-                    Macro:Disconnect()
+                        Animation:Stop()
+                        Macro =
+                            game:service'Players'.LocalPlayer.Character:findFirstChildOfClass'Humanoid'.Running:Connect(function()
+                                Animation:Stop()
+                            end)
+                    else
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "AutoMacro";
+                                Text = "AutoMacro: Off";
+                                Duration = 3;
+                            })
+                        end
+                        AutoMacroToggle = false
+                        local Greet = game.ReplicatedStorage.ClientAnimations.Greet
+                        local Humanoid = game.Players.LocalPlayer.Character.Humanoid
+                        local Animation = Humanoid:LoadAnimation(Greet)
+                        Animation:Stop()
+                        game:GetService("ReplicatedStorage"):findFirstChild("ClientAnimations"):findFirstChild("Greet"):Remove()
+                        Macro:Disconnect()
+                    end
                 end
             end
-        end
-    end)
+        end)
+    end
+
+    if Macro == true then
+        game:GetService("UserInputService").InputBegan:Connect(function(input,chatting)
+            if input.UserInputType == Enum.UserInputType.Keyboard then
+                if chatting then IsChatting = true else IsChatting = false end
+                if input.KeyCode == MacroKey then
+                    if IsChatting ~= false then return end
+                    if not MacroToggle then
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "Macro";
+                                Text = "Macro: On";
+                                Duration = 3;
+                            })
+                        end
+                        MacroToggle = true
+                        spawn(function()
+                            while MacroToggle == true do wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "I", false, game) wait(0)
+                                game:GetService("VirtualInputManager"):SendKeyEvent(true, "O", false, game) wait(0)
+                            end
+                        end)
+                    else
+                        if Notification == true then
+                            game.StarterGui:SetCore("SendNotification", {
+                                Title = "Macro";
+                                Text = "Macro: Off";
+                                Duration = 3;
+                            })
+                            MacroToggle = false
+                        end
+                    end
+                end
+            end
+        end)
+    end
 end
--- Low GFX.lua
+++ Low GFX.lua
@@ -2,7 +2,11 @@ local LowGFX = true
 
 if LowGFX == true then
     for _, Parts in pairs(workspace:GetDescendants()) do
-        if Parts:IsA("Part") then
+        if Parts:IsA("Part")
+            or Parts:IsA("SpawnLocation")
+            or Parts:IsA("WedgePart")
+            or Parts:IsA("Terrain")
+            or Parts:IsA("MeshPart") then
             if not Parts:FindFirstChild("SurfaceType") then
                 local SurfaceType = Instance.new("StringValue",Parts)
                 SurfaceType.Name = "SurfaceType"
@@ -13,7 +17,12 @@ if LowGFX == true then
     end
 elseif LowGFX == false then
     for _, Parts in pairs(workspace:GetDescendants()) do
-        if Parts:IsA("Part") and Parts:FindFirstChild("SurfaceType") then
+        if Parts:IsA("Part")
+            or Parts:IsA("SpawnLocation")
+            or Parts:IsA("WedgePart")
+            or Parts:IsA("Terrain")
+            or Parts:IsA("MeshPart")
+            and Parts:FindFirstChild("SurfaceType") then
             Parts.Material  = string.sub(Parts.SurfaceType.Value,15)
         end
     end
